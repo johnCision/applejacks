a0adf3e8c7e2c6848576e00b7aa562fe49fd2392
John Talton <john.talton@cision.com>
2021-10-22 17:29:57 -0400

    input text addition

---
+++ src/input-text.js
@@ -0,0 +1,18 @@
+//
+export class TextInput extends HTMLElement {
+	constructor() {
+		super()
+
+		const template = document.getElementById('input-text-template')
+		const { content } = template
+		const shadowRoot = this.attachShadow({ mode: 'open' })
+		shadowRoot.appendChild(content.cloneNode(true))
+	}
+
+	static get observedAttributes() { return [ 'kind' ] }
+
+	connectedCallback() {} // appended into a document
+	disconnectedCallback() {}
+	adoptedCallback() {}
+	attributeChangedCallback(_name, _oldValue, _newValue) { }
+}
