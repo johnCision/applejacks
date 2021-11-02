01a82b591c0632a7ebdccae97884a920cd6e8479
John Talton <john.talton@cision.com>
2021-11-02 14:23:20 -0400

    extend application frame

--- src/application-frame.js
+++ src/application-frame.js
@@ -1,5 +1,13 @@
+/* eslint-disable max-classes-per-file */
+
+const ATTRS = {
+	SHOW_ASIDE: 'show-aside',
+	SHOW_TOOLBAR: 'show-toolbar',
+	SHOW_CHAT: 'show-chat'
+}
+
 //
-export class ApplicationFrame extends HTMLElement {
+class ApplicationFrameCore extends HTMLElement {
 	constructor() {
 		super()
 
@@ -9,19 +17,23 @@ export class ApplicationFrame extends HTMLElement {
 		shadowRoot.appendChild(content.cloneNode(true))
 	}
 
+	static get observedAttributes() { return Object.keys(ATTRS).map(k => ATTRS[k]) }
+
+	attributeChangedCallback(_name, _oldValue, _newValue) { }
+}
+
+//
+// example of exporting attributes as properties of the object
+// this can be helpful for api interacts that are pure js
+// and/or do not want to be exposed to the core dom interface
+export class ApplicationFrame extends ApplicationFrameCore {
 	get showAside() {
-		const show = this.getAttributeNS('', 'show-aside')
+		const show = this.getAttributeNS('', ATTRS.SHOW_ASIDE)
 		return show === true
 	}
+
 	set showAside(value) {
-		if(value) { this.setAttributeNS('', 'show-aside', true); return }
-		this.removeAttributeNS('', 'show-aside')
+		if(value) { this.setAttributeNS('', ATTRS.SHOW_ASIDE, true); return }
+		this.removeAttributeNS('', ATTRS.SHOW_ASIDE)
 	}
-
-	static get observedAttributes() { return ['show-aside', 'show-toolbar'] }
-
-	connectedCallback() {} // appended into a document
-	disconnectedCallback() {}
-	adoptedCallback() {}
-	attributeChangedCallback(_name, _oldValue, _newValue) { }
 }
